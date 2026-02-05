09975e4c233f4ed816a8f186ae05ca6fddffc75b
Woonmok <qw5354@gmail.com>
2026-02-05 23:37:57 +0900

    update

---
+++ scripts/print-env.mjs
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log('TELEGRAM_BOT_TOKEN:', process.env.TELEGRAM_BOT_TOKEN);
+console.log('TELEGRAM_CHAT_ID:', process.env.TELEGRAM_CHAT_ID);
